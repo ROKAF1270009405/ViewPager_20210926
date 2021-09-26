db96fbab64c1a1aa3fe4cb455c1a510830aab96e
전상효 <junsanghyo0820@gmail.com>
2021-09-26 12:49:20 +0900

    인사말/이름/나이 표시 프래그먼트 완성

---
+++ app/src/main/java/com/jayshe/viewpager_20210926/fragments/NameFragment.kt
@@ -0,0 +1,31 @@
+package com.jayshe.viewpager_20210926.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jayshe.viewpager_20210926.R
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragement_name,container,false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        나중에 이벤트처리 작성
+
+
+    }
+
+
+}
